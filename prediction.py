aad4f8f85aae07ebdabe8d84972011cde3f1ab9e
SalimVazquez <181086@ids.upchiapas.edu.mx>
2021-07-21 09:55:37 -0500

    Predicción: Arreglando modulos  a importar

--- prediction.py
+++ prediction.py
@@ -1,6 +1,5 @@
 import numpy as np
-from keras.preprocessing.image import load_img, img_to_array
-from keras.models import load_model
+import tensorflow as tf
 
 # altura y ancho de imagenes
 height, width = 100, 100
@@ -10,14 +9,14 @@ path_model = './model/model.h5'
 path_weights = './model/weights.h5'
 
 # Cargar modelo entrenado
-cnn = load_model(path_model)
+cnn = tf.keras.models.load_model(path_model)
 cnn.load_weights(path_weights)
 
 def predict(file):
     # Cargamos una imagen
-    x = load_img(file, target_size=(width, height))
+    x = tf.keras.preprocessing.image.load_img(file, target_size=(width, height))
     # Convertimos la imagen en un arreglo
-    x = img_to_array(x)    
+    x = tf.keras.preprocessing.image.img_to_array(x)
     # Agregando una dimension extra en el eje 0 del arreglo
     x = np.expand_dims(x, axis=0)
     # Hacer una predicción, contiene un arreglo en base al No de clases
